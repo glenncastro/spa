0b2f3221dce77951229b202237e195446bac6d0a
Glenn Castro <glenn.castro@gmail.com>
2014-12-14 18:39:16 +1100

    Chapter 7 - Node.js - environments support
    Added configuration for development and production.
    For development:  $node app.js
    For production (Mac or Linux): $NODE_ENV=production node app.js

--- webapp/app.js
+++ webapp/app.js
@@ -22,9 +22,18 @@ var
 
 //---------------------------- BEGIN SERVER CONFIGURATON ----------------------------
 app.configure(function() {
-	app.use(express.logger());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+});
+app.configure('development', function() {
+	app.use(express.logger());
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
+});
+app.configure('production', function() {
+	app.use(express.errorHandler());
 });
 app.get('/', function(request, response) {
 	response.send('Hello Express');

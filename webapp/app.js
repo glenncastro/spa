af47a390fe3a81eb02c98af897beb113d7372392
Glenn Castro <glenn.castro@gmail.com>
2014-12-16 18:59:43 +1100

    Chapter 8 - MongoDB - add CRUD

--- webapp/app.js
+++ webapp/app.js
@@ -1,5 +1,5 @@
 /*
- * app.js - Express server with basic auth
+ * app.js - Express server with routing
 */
 
 /*jslint          node: true,          continue: true,
@@ -24,7 +24,6 @@ var
 app.configure(function() {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(express.basicAuth('user', 'spa'));
 	app.use(express.static(__dirname + '/public'));
 	app.use(app.router);
 });
